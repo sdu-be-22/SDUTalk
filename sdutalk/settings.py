098261feddc33ca6cf4f788f1c7e614588812490
Darkhan <200103208@stu.sdu.edu.kz>
2022-05-06 03:06:18 +0600

    fixed problem with password reset

--- sdutalk/settings.py
+++ sdutalk/settings.py
@@ -137,3 +137,9 @@ STATICFILES_DIRS = [
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sdutalkreset@gmail.com'
+EMAIL_HOST_PASSWORD = 'SdutalkReset123'
